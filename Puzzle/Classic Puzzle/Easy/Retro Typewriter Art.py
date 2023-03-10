c9a6ce42424d1a0d7a690ea000b7ab9c2f0e633b
Kaj Nielsen <kajrnielsen@protonmail.com>
2023-03-10 22:37:46 +0100

    Final Code

--- Puzzle/Classic Puzzle/Easy/Retro Typewriter Art.py
+++ Puzzle/Classic Puzzle/Easy/Retro Typewriter Art.py
@@ -8,22 +8,26 @@ abr = {
 }
 t = ['1sp', '1/', '1bS', '1_', '1/', '1bS', 'nl', '1(', '1sp', '1o', '1.', '1o', '1sp', '1)', 'nl', '1sp', '1>', '1sp', '1^', '1sp', '1<', 'nl', '2sp', '3|']
 
-
+print("-----------------------------")
 for i in t:
-    # print(i)
-    if i in abr:
-        print(abr[i])
     if i.isdigit():
         j, k = i[:-1], i[-1]
-        print(int(j) * k)
+        print(int(j) * k, end='')
     count = 0
+    if not i[0].isdigit():
+        if i in abr:
+            print(abr[i], end='')
+            continue
+        else:
+            print(i, end='')
     for le in range(len(i)):
-        
-        if i[le].isdigit():
+        if not i[le].isdigit():
+            break
+        else:
             count += 1
 
     m, n = i[:count], i[count:]
     if n in abr:
-        print(int(m) * abr[n])
+        print(int(m) * abr[n], end='')
     else:
-        print(int(n) * m)   
+        print(int(m) * n, end='')
